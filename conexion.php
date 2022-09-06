669392f00b3db7496ee1aa5a62cec1c3db29ad02
Andres Diago <andresdiag@unicauca.edu.co>
2022-09-06 00:03:32 -0500

    Commit inicial

---
+++ conexion.php
@@ -0,0 +1,9 @@
+<?php
+    $host = "localhost";
+    $user = "root";//"u715763332_admin";
+    $pw   = "";//"Lab4unicauca";
+    $db   = "db_biodigester";//"u715763332_db_biodigester";
+
+    $conectar = mysqli_connect($host, $user, $pw, $db);
+
+?>
